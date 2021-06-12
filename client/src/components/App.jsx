cf238c353f3b7471e67b1d95e2cd273302702101
jeffvli <jeffvictorli@gmail.com>
2021-06-12 04:44:15 -0700

    Add initial routes

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,16 +1,28 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Navbar from "./navbar/Navbar";
 import Footer from "./footer/Footer";
+import PollCreator from "./poll/PollCreator";
 import Poll from "./poll/Poll";
+import MissingPage from "./missingpage/MissingPage";
 
 function App() {
   return (
     <>
       <Router>
         <Navbar />
-        <Poll></Poll>
         <Footer />
+        <Switch>
+          <Route exact path="/polls/:pollId">
+            <Poll></Poll>
+          </Route>
+          <Route exact path="/">
+            <PollCreator></PollCreator>
+          </Route>
+          <Route path="*">
+            <MissingPage></MissingPage>
+          </Route>
+        </Switch>
       </Router>
     </>
   );
