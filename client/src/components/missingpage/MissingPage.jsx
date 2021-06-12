3822d6d3b99523c134d0288c6bff145d4a32a53d
jeffvli <jeffvictorli@gmail.com>
2021-06-12 04:42:59 -0700

    Add 404 page

---
+++ client/src/components/missingpage/MissingPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link, Container, Box, Text } from "@chakra-ui/layout";
+import { Link as RouterLink } from "react-router-dom";
+
+const MissingPage = () => {
+  return (
+    <Box ml={5}>
+      <Text fontSize="xl">You have reached an invalid page</Text>
+      <Link as={RouterLink} to="/">
+        Return home
+      </Link>
+    </Box>
+  );
+};
+
+export default MissingPage;
