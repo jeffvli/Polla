60c4960e3a2f4090bd9805ddc64ddc734da9d529
jeffvli <jeffvictorli@gmail.com>
2021-06-19 01:04:17 -0700

    Update client jwt integration, refresh tokens

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,11 +1,45 @@
 import axios from "axios";
 import useSWR from "swr";
+import createAuthRefreshInterceptor from "axios-auth-refresh";
+
+const refreshAuthLogic = async () => {
+  await axios
+    .post(`${process.env.REACT_APP_API_URL}/auth/token`, {
+      refreshToken: localStorage.getItem("refreshToken"),
+    })
+    .then((tokenRefreshResponse) => {
+      localStorage.setItem("token", tokenRefreshResponse.data.token);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 export const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  baseURL: process.env.REACT_APP_API_URL,
+  headers: {
+    Authorization: "Bearer " + localStorage.getItem("token"),
+  },
 });
 
+export const authApi = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+  headers: {
+    Authorization: "Bearer " + localStorage.getItem("token"),
+  },
+});
+
+createAuthRefreshInterceptor(api, refreshAuthLogic);
+
 export const fetcher = (url) => api.get(url).then((res) => res.data);
+export const authFetcher = (url, token) =>
+  api
+    .get(url, {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    })
+    .then((res) => res.data);
 
 export const usePolls = (refreshInterval) => {
   const { data, error } = useSWR(`/polls/`, fetcher, {
@@ -27,6 +61,18 @@ export const usePoll = (slug) => {
 
   return {
     poll: data,
+    isLoading: !error && !data,
+    isError: error,
+  };
+};
+
+export const useUser = (token) => {
+  const { data, error } = useSWR([`/auth/user`, token], authFetcher, {
+    revalidateOnFocus: false,
+  });
+
+  return {
+    user: { isAuthenticated: !error, data: data },
     isLoading: !error && !data,
     isError: error,
   };
