731e2a531c57f340303e36ccd314d80a369e2705
jeffvli <jeffvictorli@gmail.com>
2021-06-30 03:11:57 -0700

    Move 401 check to middleware

--- server/middleware/checkAuthenticated.js
+++ server/middleware/checkAuthenticated.js
@@ -1,8 +1,13 @@
 const passport = require("passport");
+const errorMessage = require("../utils/errorMessage");
 
 const checkAuthenticated = function (req, res, next) {
   passport.authenticate("jwt", { session: false }, function (err, user, info) {
     req.authenticated = !!user;
+
+    if (!req.authenticated && req.headers.authorization) {
+      return res.status(401).json(errorMessage(401, "Invalid authorization."));
+    }
 
     if (user) {
       req.user = { id: user.id, username: user.username };
